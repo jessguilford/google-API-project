4c22ce7a8517f847f40b265440007dbab5cd26e0
Christine Tacha and Jess Guilford <tacha.christine@gmail.com>
2016-03-16 11:04:05 -0700

    make Portland, OR the center of the map

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 0, lng: 0},
-    zoom: 3,
+    center: {lat: 45.5324167, lng: -122.655503},
+    zoom: 13,
     styles: [{
       featureType: 'poi',
       stylers: [{ visibility: 'off' }]  // Turn off points of interest.
