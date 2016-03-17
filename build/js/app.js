5d043dc21e2e049616aeaacd5788d4ba742147b8
Christine Tacha and Jess Guilford <jess.guilford@gmail.com>
2016-03-17 16:12:41 -0700

    add basic styling

--- build/js/app.js
+++ build/js/app.js
@@ -62,6 +62,13 @@ var triple = new Bar("Triple Nickel", 45.5175688, -122.6261492, "5-2:30am");
 var mfp = new Bar("My Father's Place", 45.5188379, -122.6609757, "5-2:30am");
 var spaceroom = new Bar("Space Room", 45.5118617, -122.615097, "5-2:30am");
 var binks = new Bar("Binks", 45.5591568, -122.6396719, "12-2:30am");
+var paddys = new Bar("Paddys", 45.5171232, -122.6756239, "3-2:30am");
+var kellysOlympian = new Bar("Kelly's Olympian", 45.5198923, -122.6787462, "2-2:30am");
+var rialto = new Bar("Rialto", 45.5194304, -122.6784697, "5-2:30am");
+var sideStreet = new Bar("Side Street", 45.5167138, -122.6316498, "3-2:30am");
+var sweetHereafter = new Bar("Sweet Hereafter", 45.5163145, -122.6325297, "4-2:30am");
+var belmontInn = new Bar("The Belmont Inn", 45.5164566, -122.6271368, "3-2:30am");
+
 
 $(document).ready(function() {
   var markerArray = [];
