b5296c91e86fcbfb0eab8f05dfe1686eefd52eb9
Jessie Guilford <Jessie Guilford>
2016-03-20 11:47:17 -0700

    add polyline route functionality

--- js/map-interface.js
+++ js/map-interface.js
@@ -1,12 +1,7 @@
 var Bar = require('./../js/bar-crawl.js').Bar;
-// var markerArray = require('./../js/browser-interface.js').markerArray;
-
-// function addToArray(bar) {
-//   markerArray.push(bar);
-//   return markerArray;
-// }
 
 exports.addMapMarkers = function(markerArray) {
+  var crawlCoordinates = [];
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 45.516326, lng: -122.655503},
     zoom: 12,
@@ -21,28 +16,31 @@ exports.addMapMarkers = function(markerArray) {
   });
 
   for(var i = 0; i < markerArray.length; i++) {
-    console.log("getting markerArray");
     var thisLat = markerArray[i].latitude;
     var thisLong = markerArray[i].longitude;
     var thisName = markerArray[i].name;
-    console.log(thisLat);
+
     var marker = new google.maps.Marker({
       position: {lat: thisLat, lng: thisLong},
       title: thisName,
       map: map
     });
-    var infowindow = new google.maps.InfoWindow({
-    content: this.title,
-    });
-    // marker.addListener("click", function() {
-    //   alert(this.title);
-    // });
     marker.addListener("click", function() {
-      infowindow.open(map, marker)
+      infowindow = new google.maps.InfoWindow({
+          content: this.title
+        });
+      infowindow.open(map,this);
     });
+    crawlCoordinates.push(marker.getPosition());
   }
-
   console.log("rendered map markers");
+  var crawlPath = new google.maps.Polyline({
+    map: map,
+    path: crawlCoordinates,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
 };
 
 // exports.addMapMarkers = addMapMarkers;
